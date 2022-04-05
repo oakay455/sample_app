00167ca8a1ea4a3ef7fb6f8b3e1c8b1c182ba68c
oakay455 <ayako110.89@gmail.com>
2022-04-05 17:33:15 +0000

    [Finish]3-2

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,12 +1,12 @@
 class ListsController < ApplicationController
-  
+
   def new
     @list = List.new
   end
 
 def create
   list = List.new(list_params)
-  
+
 list.save
   redirect_to '/top'
 end
@@ -19,9 +19,9 @@ end
 
   def edit
   end
-  
+
   private
-  
+
   def list_params
     params.require(:list).permit(:title,:body)
   end
